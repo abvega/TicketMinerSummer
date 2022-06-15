b96fccccf978fa735a6e260a4afdeddf2a29a397
alexvega <abvega@miners.utep.edu>
2022-06-15 14:43:08 -0600

    update with new javafx stuff added

--- src/RunTicketMiner.java
+++ src/RunTicketMiner.java
@@ -1,16 +1,10 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class RunTicketMiner extends Application{
         public static void main(String[] args){
@@ -19,7 +13,7 @@ public class RunTicketMiner extends Application{
         @Override
         public void start(Stage primaryStage) throws IOException {
             try{
-                Parent root = FXMLLoader.load(getClass().getResource("WelcomeScreen.fxml"));
+                Parent root = FXMLLoader.load(getClass().getResource("MainMenuGUI.fxml"));
                 Scene scene1 = new Scene(root);
                 primaryStage.setScene(scene1);
                 primaryStage.sizeToScene();
--- src/UserList.java
+++ src/UserList.java
@@ -1,112 +1,25 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class UserList {
     private static UserList instance;
     private final MakeUser makeUser = new MakeUser();
-    private final ArrayList<User> users = makeUser.userFromCSV("CustomerListPA4FINAL.csv");
+    private final HashMap<String, User> users = makeUser.userFromCSV("CustomerListPA4FINAL.csv");
     private final Scanner in = new Scanner(System.in);
 
-    public static UserList getInstance(){
-        if(instance == null){
+    public static UserList getInstance() {
+        if (instance == null) {
             instance = new UserList();
         }
         return instance;
     }
-
-
-    public void modifyUser(User user){
-        int input;
-        do {
-            try {
-                System.out.println("""
-                        How would you like to modify your user profile?.\s
-                        1.User Name\s
-                        2.First Name\s
-                        3.Last Name\s
-                        4.Password\s
-                        """);
-                input = Integer.parseInt(in.nextLine());
-                switch (input) {
-                    case 1 -> chngUserName(user);
-                    case 2 -> chngFrstNme(user);
-                    case 3 -> chngLstNme(user);
-                    case 4 -> chngPssWrd(user);
-                    default -> throw new IllegalArgumentException("Please use one of the provided options");
-                }
-            } catch (IllegalArgumentException i) {
-                System.out.println("Message " + i);
-            }
-        System.out.println("Keep editing profile? Y or N ");
-        }while(in.nextLine().equalsIgnoreCase("Y"));
+    public boolean userExist(String userName) {
+        return (users.containsKey(userName));
     }
-    public void chngUserName(User user){
-        String input;
-        System.out.println("What would you like to change your user name to? ");
-        input = in.nextLine();
-        user.setIserName(input);
-    }
-    public void chngPssWrd(User user){
-        String input;
-        while(true) {
-            System.out.println("Input your current password");
-            input = in.nextLine();
-            if (user.getPassWord().equalsIgnoreCase(input)) {
-                System.out.println("Please type in your new password");
-                user.setPassWord(in.nextLine());
-                return;
-            } else {
-                System.out.println("That password is incorrect.");
-            }
+    public User getUser(String userName) {
+        if (users.get(userName) == null) {
+            return null;
         }
+        return users.get(userName);
     }
-
-    /**
-     *
-     * @param user
-     * user passed to check membership status
-     * @param amt
-     *if user is member, 10% is applied to amt
-     */
-    public void chngBalance(User user, double amt) {
-        if (chckBlnce(user, amt)) {
-            if (user.getIsMember()) {
-                amt = amt * .1;
-            }
-            user.setMoneyTotal(amt);
-        }
-        else{
-            System.out.println("User doesn't have enough money in this account");
-        }
-    }
-    public void chngFrstNme(User user){
-        System.out.println("What would you like to change this user's first name to?");
-        user.setFirstName(in.nextLine());
-        System.out.println("User's name has been change to " + user.getFirstName());
-    }
-    public void chngLstNme(User user){
-        System.out.println("What would you like to change this user's first name to?");
-        user.setLastName(in.nextLine());
-        System.out.println("User's last name has been changed to "+ user.getLastName());
-    }
-    public void updtTix(User user, int tix){
-        user.setTixBought(tix);
-    }
-    public void chngMem(User user){
-        user.setMember(!user.getIsMember());
-    }
-    public boolean chckBlnce(User user, double total){
-        return (user.getMoneyTotal() > total);
-    }
-
-    /**
-     *
-     * @param userName
-     * @param passW
-     * @return boolean
-     *  accepts user input of username and password.returns true if the username exists as well as the
-     *  password matching what user input.
-     */
-
 }
